a63004e7a456f61a38d325291a99846ea28b6d20
Sanjay Saatyaki <Saatyaki.Sanjay@in.bosch.com>
2022-01-09 23:37:34 +0530

    Implementation of Printing Reference manual

--- even_count_color_generator.py
+++ even_count_color_generator.py
@@ -1,4 +1,5 @@
 from color_codes import MAJOR_COLORS,MINOR_COLORS
+import color_codes as colors
 
 def get_color_from_pair_number(pair_number):
   '''
@@ -15,13 +16,11 @@ def get_color_from_pair_number(pair_number):
     raise Exception('Minor index out of range')
   return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
 
-
 def get_pair_number_from_color(major_color, minor_color):
   '''
   Description : Based on the even count color code it will give the pair number for the major and minor colors
   Input Params : major_color, minor_color (str)
   Output Params : pair_number
-  
   '''  
   try:
     major_index = MAJOR_COLORS.index(major_color)
@@ -32,3 +31,19 @@ def get_pair_number_from_color(major_color, minor_color):
   except ValueError:
     raise Exception('Minor index out of range')
   return major_index * len(MINOR_COLORS) + minor_index + 1
+
+def print_color_code():
+  print("Reference Manual")
+  print("{:<15} {:<15}".format('Pair Number', 'Major Minor ColorPairs'))
+  color_codes = get_all_color_codes()
+  for color_code in color_codes:
+    print(color_code)
+
+def get_all_color_codes():
+  color_codes = []
+  no_of_pairs = len(MAJOR_COLORS)*len(MINOR_COLORS)+1
+  for pair_number in range(1,no_of_pairs):
+    major_color , minor_color = get_color_from_pair_number(pair_number)
+    color_pair_code = colors.color_pair_to_string(major_color,minor_color)
+    color_codes.append("{:<15} {:<15} ".format(pair_number, color_pair_code))
+  return color_codes
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from even_count_color_generator_test import excute_testcases
-
+import even_count_color_generator as generator
 
 if __name__ == '__main__':
   excute_testcases()
+  generator.print_color_code()
   print('Done :)')
